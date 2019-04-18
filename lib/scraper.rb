ea622e20a476a290cc55ab135467e36f3439f574
Natasha Chernis <natashachernis@gmail.com>
2019-04-18 02:59:22 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,9 +1,21 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 require_relative './course.rb'
 
 class Scraper
+
+  
+  def get_page
+    
+  end
+
+  def get_courses
+  end
+
+  def make_courses
+  end
 
   def print_courses
     self.make_courses
@@ -16,13 +28,5 @@ class Scraper
     end
   end
 
-  def get_page
-  end
-
-  def get_courses
-  end
-
-  def make_courses
-  end
 
 end
