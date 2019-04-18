e140b9e2e95707834381e3b0b42705e08a3c2dc1
Natasha Chernis <natashachernis@gmail.com>
2019-04-18 02:57:37 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,7 +5,7 @@ require_relative './course.rb'
 
 class Scraper
   
-    def print_courses
+  def print_courses
     self.make_courses
     Course.all.each do |course|
       if course.title
@@ -14,6 +14,15 @@ class Scraper
         puts "  Description: #{course.description}"
       end
     end
+  end
+
+  def get_page 
+  end
+
+  def get_courses 
+  end 
+
+  def make_courses 
   end
   
 end
